4fbf545453426d7d6b9b986cd0b4a8ffa1acbdd3
hpessien <49365209+hpessien@users.noreply.github.com>
2021-07-12 15:04:38 +0200

    Update LocatingElement.py
    comment added

--- LocatingElement.py
+++ LocatingElement.py
@@ -11,6 +11,8 @@ driver.get("https://techwithtim.net")
 #driver.close()
 print(driver.title)
 
+#just adding this comment
+
 search = driver.find_element_by_name("s")
 search.send_keys("test")
 search.send_keys(Keys.RETURN )
